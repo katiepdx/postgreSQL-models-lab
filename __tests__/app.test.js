d4dfd9a1f7c7b8896baae6bc9f9af6728161114c
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-08 21:47:15 -0700

    add GET by id route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Phone = require('../lib/models/phones.js');
+const { report } = require('../lib/app.js');
 
 describe('Phones routes', () => {
     beforeEach(() => {
@@ -21,6 +23,68 @@ describe('Phones routes', () => {
 
         expect(response.body).toEqual({
             id: expect.any(Number),
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,449.99'
+        });
+    });
+
+    it.skip('updates an existing phone by id using the PUT route', async () => {
+        // Create a new phone (to update later)
+        const newPhone = await Phone.insert({
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,449.99'
+        });
+
+        const response = await request(app)
+            .put(`/api/phones/${newPhone.id}`)
+            .send({
+                name: 'Samsung Note 20 Ultra 512gb',
+                brand: 'Samsung',
+                cost: '$1,000.99'
+            });
+
+        expect(response.body).toEqual({
+            id: newPhone.id,
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,000.99'
+        });
+    });
+
+    it('deletes an existing phone by id using the DELETE route', async () => {
+        // Create a new phone (to delete later)
+        const newPhone = await Phone.insert({
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,449.99'
+        });
+
+        const response = await request(app)
+            .delete(`/api/phones/${newPhone.id}`);
+
+        expect(response.body).toEqual({
+            id: newPhone.id,
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,449.99'
+        });
+    });
+
+    it('gets an existing phone by id using the GET route', async () => {
+        // Create a new phone (to get later)
+        const newPhone = await Phone.insert({
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,449.99'
+        });
+
+        const response = await request(app)
+            .get(`/api/phones/${newPhone.id}`);
+
+        expect(response.body).toEqual({
+            id: newPhone.id,
             name: 'Samsung Note 20 Ultra 512gb',
             brand: 'Samsung',
             cost: '$1,449.99'
