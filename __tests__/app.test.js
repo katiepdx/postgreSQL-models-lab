eed7da071e9ead884a7bb243dff687c305a76bc8
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-08 20:42:53 -0700

    add POST route test

---
+++ __tests__/app.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable space-before-function-paren */
+const fs = require('fs');
+const pool = require('../lib/utils/pool.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+const Phone = require('../lib/models/phones.js');
+
+describe('Phones routes', () => {
+    beforeEach(() => {
+        return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+    });
+
+    it('creates a new phone using the POST route', async () => {
+        // wait for response from our app
+        const response = await request(app)
+            .post('/api/phones')
+            .send({
+                name: 'Samsung Note 20 Ultra 512gb',
+                brand: 'Samsung',
+                cost: '$1,449.99'
+            });
+
+        expect(response.body).toEqual({
+            id: expect.any(Number),
+            name: 'Samsung Note 20 Ultra 512gb',
+            brand: 'Samsung',
+            cost: '$1,449.99'
+        });
+    });
+});
